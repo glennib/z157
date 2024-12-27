27ad4b51f4421743753536983ebc132d11ab3b82
Glenn Bitar <glennbitar@gmail.com>
2024-12-27 13:43:06 +0100

    perf: Increase sample size and sample time for benchmarking

--- benches/parse.rs
+++ benches/parse.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use criterion::Criterion;
 use criterion::black_box;
 use criterion::criterion_group;
@@ -8,10 +10,17 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("parse_large_input", |b| {
         b.iter(|| Tree::parse(black_box(include_str!("large-input.txt"))));
     });
+
     c.bench_function("parse_small_input", |b| {
         b.iter(|| Tree::parse(black_box(include_str!("small-input.txt"))));
     });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(
+    name = benches;
+    config = Criterion::default()
+        .measurement_time(Duration::from_secs(10))
+        .sample_size(100_000);
+    targets = criterion_benchmark
+);
 criterion_main!(benches);
